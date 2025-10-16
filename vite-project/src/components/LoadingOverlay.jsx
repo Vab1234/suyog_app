f387c0f788013877291720510769ad6ccb89ea16
Varun Budhani <111705556+Vab1234@users.noreply.github.com>
2025-10-16 09:25:30 +0530

    Delete vite-project directory

--- vite-project/src/components/LoadingOverlay.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const LoadingOverlay = () => (
-  <div className="loading-overlay active">
-    <div className="pan-animation"></div>
-  </div>
-);
-
-export default LoadingOverlay;
